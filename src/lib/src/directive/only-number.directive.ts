4037ffdc6d036c9871fe73a3d47a2889f0b72af2
Constantin Harea <hareacostica@gmail.com>
2018-05-02 19:19:32 +0300

    Updated only-numbers directive to work properly & allow plus only at begining and once

--- src/lib/src/directive/only-number.directive.ts
+++ src/lib/src/directive/only-number.directive.ts
@@ -8,38 +8,37 @@ export class OnlyNumberDirective {
     @Input()
     onlyNumber: boolean;
 
-    regexStr = '^[0-9]*$';
-
     constructor(private el: ElementRef) {
     }
 
     @HostListener('keydown', ['$event']) onKeyDown(event: KeyboardEvent) {
         let e = <KeyboardEvent> event;
         if (this.onlyNumber) {
+            if ((e.key === '+' || e.keyCode === 107 || (e.shiftKey && e.keyCode === 187)) && (
+                this.el.nativeElement.value.indexOf('+') > -1 ||
+                this.el.nativeElement.value.length > 0
+            )) {
+                e.preventDefault();
+                return;
+            }
+
             if (
                 [46, 8, 9, 27, 13, 110, 190, 171].indexOf(e.keyCode) !== -1 ||
-                // Allow: Ctrl+A
-                (e.keyCode === 65 && e.ctrlKey === true) ||
-                // Allow: Ctrl+C
-                (e.keyCode === 67 && e.ctrlKey === true) ||
-                // Allow: Ctrl+V
-                (e.keyCode === 86 && e.ctrlKey === true) ||
-                // Allow: Ctrl+X
-                (e.keyCode === 88 && e.ctrlKey === true) ||
-                // Allow: home, end, left, right
-                (e.keyCode >= 35 && e.keyCode <= 39) ||
+                // Allow: Ctrl+A|X|C|V, Command+A|X|C|V
+                ([65, 88, 67, 86].indexOf(e.keyCode) !== -1 && (e.ctrlKey === true || e.metaKey === true)) ||
+                // Allow: home, end, left, right, down, up
+                (e.keyCode >= 35 && e.keyCode <= 40) ||
                 // Allow plus button
-                (e.keyCode === 107) || (e.keyCode === 187)) {
+                (e.key === '+' || e.keyCode === 107 || (e.shiftKey && e.keyCode === 187))) {
                 // let it happen, don't do anything
                 return;
             }
-            let ch = String.fromCharCode(e.keyCode);
-            let regEx = new RegExp(this.regexStr);
-            if (regEx.test(ch)) {
-                return;
-            } else if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+
+            // Ensure that it is a number and stop the keypress
+            if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
                 e.preventDefault();
             }
+
         } else {
             return;
         }
