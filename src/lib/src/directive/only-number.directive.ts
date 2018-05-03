99ce25176b35ac03ccf871e0cd17dff0464b53e6
Constantin Harea <hareacostica@gmail.com>
2018-05-03 10:19:33 +0300

    Improved  &  block

--- src/lib/src/directive/only-number.directive.ts
+++ src/lib/src/directive/only-number.directive.ts
@@ -14,14 +14,6 @@ export class OnlyNumberDirective {
     @HostListener('keydown', ['$event']) onKeyDown(event: KeyboardEvent) {
         let e = <KeyboardEvent> event;
         if (this.onlyNumber) {
-            if ((e.key === '+' || e.keyCode === 107 || (e.shiftKey && e.keyCode === 187)) && (
-                this.el.nativeElement.value.indexOf('+') > -1 ||
-                this.el.nativeElement.value.length > 0
-            )) {
-                e.preventDefault();
-                return;
-            }
-
             if (
                 [46, 8, 9, 27, 13, 110, 190, 171].indexOf(e.keyCode) !== -1 ||
                 // Allow: Ctrl+A|X|C|V, Command+A|X|C|V
@@ -29,13 +21,31 @@ export class OnlyNumberDirective {
                 // Allow: home, end, left, right, down, up
                 (e.keyCode >= 35 && e.keyCode <= 40) ||
                 // Allow plus button
-                (e.key === '+' || e.keyCode === 107 || (e.shiftKey && e.keyCode === 187))) {
+                (e.key === '+' || e.keyCode === 107 || e.keyCode === 187)) {
                 // let it happen, don't do anything
                 return;
             }
 
             // Ensure that it is a number and stop the keypress
             if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+                e.preventDefault();
+            }
+
+        } else {
+            return;
+        }
+    }
+
+    @HostListener('keypress', ['$event']) onKeyPress(event: KeyboardEvent) {
+        let e = <KeyboardEvent> event;
+        if (this.onlyNumber) {
+            if (
+                (e.key === '=' || String.fromCharCode(e.keyCode) === '=') || (
+                    (e.key === '+' || String.fromCharCode(e.keyCode) === '+') && (
+                        this.el.nativeElement.value.indexOf('+') > -1 ||
+                        this.el.nativeElement.value.length > 0
+                    )
+            )) {
                 e.preventDefault();
             }
 
